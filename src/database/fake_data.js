edc4b34527b597e751a530c79e0822bd7af616ac
Bruno Vargas <brunovargas7899@gmail.com>
2020-08-08 13:41:28 -0300

    done

---
+++ src/database/fake_data.js
@@ -0,0 +1,28 @@
+//Dados
+
+const proffys = [
+    { 
+    name: "Diego Fernandes", 
+    avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+    whatsapp: "8998765434", 
+    bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões." , 
+    subject: "Química", 
+    cost:"20", 
+    weekday: [0], 
+    time_from: [720], 
+    time_to: [1220]
+    },
+    { 
+        name: "Bruno Vargas", 
+        avatar: "https://avatars2.githubusercontent.com/u/62037302?s=460&u=5ab073c8e742ff75b86ae518f4a369d5dc09bf4f&v=4", 
+        whatsapp: "8998765434", 
+        bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões." , 
+        subject: "Química", 
+        cost:"20", 
+        weekday: [1], 
+        time_from: [720], 
+        time_to: [1220]
+        }
+
+        
+]
